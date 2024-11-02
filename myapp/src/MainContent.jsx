f63bfa05176a2d6b949d726c26b6447fc2fd6d4a
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2024-11-02 11:31:59 +0700

    Learn react how to use useEffect

--- myapp/src/MainContent.jsx
+++ myapp/src/MainContent.jsx
@@ -1,16 +1,17 @@
-import { StrictMode, useState } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import "./index.css";
 
 function MainContent() {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  return (
-    <div>
-      <h3>Tambahkan nilai saat ini {count}</h3>
-      <button onClick={increment}>Tambahkan</button>
-    </div>
-  );
+  const [second, setSecond] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecond((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return <p>Detik saat ini {second} second</p>;
 }
 export default MainContent;
